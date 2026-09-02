15b6fba260e9c4ad38d5a464822e1b407f2f0e0d
aninhajc76-spec <aninhajc76@gmail.com>
2026-09-02 20:17:42 -0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,7 +1,7 @@
-const SUPABASE_URL = https://tfzzzchrbntvzutfskyn.supabase.co/rest/v1/
-const SUPABASE_ANON_KEY = sb_publishable_R7E5xXV611vFqkWWY5qwcg_wtW3ERZO
+const SUPABASE_URL = "https://tfzzzchrbntvzutfskyn.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_R7E5xXV611vFqkWWY5qwcg_wtW3ERZO";
 
-const supabaseClient = window.supabase.createClient(
+window.supabaseClient = window.supabase.createClient(
   SUPABASE_URL,
   SUPABASE_ANON_KEY
 );
