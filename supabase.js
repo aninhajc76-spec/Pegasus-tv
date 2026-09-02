be6c2ca0d3830c3793b9d87b26029554bb95ba2b
aninhajc76-spec <aninhajc76@gmail.com>
2026-09-02 16:20:40 -0300

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL =https://tfzzzchrbntvzutfskyn.supabase.co
+const SUPABASE_ANON_KEY =sb_publishable_R7E5xXV611vFqkWWY5qwcg_wtW3ERZO
+
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
